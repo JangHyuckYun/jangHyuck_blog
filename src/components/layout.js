055cc29e094ddc198b10c1b8250c23eaa74b1382
by09019 <skg09203@naver.com>
2022-03-02 15:07:06 +0900

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Link} from "gatsby";
 import Logo from "../images/logo_blue.png";
-import "../scss/layout.scss";
+import "./../scss/layout.scss";
 import Utterances from "./utterances";
 
 const kebabCase = (str) => str.split(" ").join("-").toLowerCase();
