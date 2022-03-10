4da191426df042de43fac895291712e8e5de8817
by09019 <skg09203@naver.com>
2022-03-10 23:01:48 +0900

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,8 +21,7 @@ const Category = ({categories, thisCategory}) => {
     // ex) const refineCategories = [{ fieldValue: "category/subCategory, totalCount: 2 }]
     // => [ { fieldValue: "category", totalCount:2, subCategory: [ { fieldValue: "subCategory", totalCount: 2, subCategory:[] } ]  } ]
     // 으로 변환되게 만들었음.
-    // 아직 변수명을 잘 못지어서 고심 끝에 짓긴 하였지만 이상할 수 있음.
-    console.log(categories);
+    // 아직 변수명을 잘 못지어서 고심 끝에 짓긴 하였지만 이상할 수 있음
     const refineCategoies = categories.reduce((acc, { fieldValue, totalCount }, idx) => {
         const fFirstObjIdx = (value) => acc.findIndex((data, dIdx) => data.fieldValue !== undefined && data.fieldValue === value);
         const categoryArr = fieldValue.split("/");
@@ -37,18 +36,22 @@ const Category = ({categories, thisCategory}) => {
             // 0번째는 이미 mainTarget에 있으므로 0번째는 넘김.
             if(dIdx === 0) return true;
 
-            const subTargetIdx = subTarget.subCategory.findIndex((d) => d.fieldValue !== undefined && d.fieldValue === category);
+            let subTargetIdx = subTarget?.subCategory.findIndex((d) => d.fieldValue !== undefined && d.fieldValue === category) || -1;
 
             // subTarget에 해당 카테고리가 없으면 새로 생성
             // 해당 카테고리가 있으면 totalCount 1씩 증가
             if(subTargetIdx === -1) {
                 const categoryPath = [...categoryArr].slice(0, dIdx + 1).join("/");
                 subTarget.subCategory.push({ fieldValue:category, totalCount, subCategory:[], categoryPath });
+
+                subTargetIdx = 0;
             } else {
                 subTarget.totalCount += 1;
             }
 
-            mainTarget.totalCount += 1;
+            if(subTarget.fieldValue !== mainTarget.fieldValue) {
+                mainTarget.totalCount += 1;
+            }
 
             // subTarget을 해당 카테고리의 서브 카테고리 값으로 변경
             subTarget = subTarget.subCategory[subTargetIdx];
